9f811be3f2d23a6eab9cc9bb174d62c1e7421387
DavidLineman <dline215@gmail.com>
2019-06-07 14:09:51 -0400

    implemente dfunctional buttons for employee homepage confirmations

--- app/policies/audit_log_policy.rb
+++ app/policies/audit_log_policy.rb
@@ -1,12 +1,20 @@
 class AuditLogPolicy < ApplicationPolicy
   def index?
+    #TODO Refactor
     return true if admin?
   end
+
+  def confirm?
+    record.user_id == user.id
+  end
+
 
   private 
 
   def admin?
     admin_types.include?(user.type)
   end
+
+  
   
 end
